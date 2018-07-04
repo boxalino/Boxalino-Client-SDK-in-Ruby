14d13419e554b0df4075b3bca890a7d4785f5c96
nextolivedev <aftab@nextolive.com>
2018-07-04 20:41:21 +0530

    code fixes for addFieldParameter
    code fixes for addFieldParameter

--- lib/BxData.rb
+++ lib/BxData.rb
@@ -322,7 +322,7 @@ class BxData
             raise s"trying to add a field parameter on sourceId 'sourceId', container 'container', fieldName 'fieldName' while this field doesn't exist"
         end
         if(@sources[container][sourceId]['fields'][fieldName]['fieldParameters']== nil)
-            @sources[container][sourceId]['fields'][fieldName]['fieldParameters'] = Array.new
+            @sources[container][sourceId]['fields'][fieldName]['fieldParameters'] = Hash.new
         end
         @sources[container][sourceId]['fields'][fieldName]['fieldParameters'][parameterName] = parameterValue
     end
